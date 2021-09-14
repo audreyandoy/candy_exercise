c7d1cf5cd89deb8f8e28dacd396b48d7ccc43ffc
Audrey Andoy <audreylandoy@gmail.com>
2021-09-13 18:21:47 -0700

    solution for dictionary with list values

--- tests/test_candy_data_structure.py
+++ tests/test_candy_data_structure.py
@@ -2,7 +2,6 @@ import pytest
 from candy_problem.main import * 
 
 def test_create_candy_data_structure():
-    pass
 
     # Arrange
     friend_favorites = [
@@ -16,4 +15,47 @@ def test_create_candy_data_structure():
     new_candy_data = create_new_candy_data_structure(friend_favorites)
     
     # Assert
+    assert type(new_candy_data) == dict   
+    assert len(new_candy_data.keys()) == 8
+    assert new_candy_data["lollipop"] == ["Sally", "Bob"]
+
+
+def test_get_friends_who_like_candy_type():
+    # Arrange
+    friend_favorites = [
+        ["Sally", [ "lollipop”, “bubble gum", "laffy taffy"]],
+        [ "Bob", ["milky way", "licorice", "lollipop"]],
+        [ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+        [ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+    ]
     
+    data = create_new_candy_data_structure(friend_favorites)
+    candy_type = "lollipop"
+
+    # Act
+    result = get_friends_who_like_candy_type(data, candy_type)
+
+    # Assert
+    assert result == ["Sally", "Bob"]
+    assert len(result) == 2
+    assert type(result) == list 
+
+
+def test_create_candy_set():
+
+    # Arrange
+    friend_favorites = [
+        ["Sally", [ "lollipop”, “bubble gum", "laffy taffy"]],
+        [ "Bob", ["milky way", "licorice", "lollipop"]],
+        [ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+        [ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+    ]
+    
+    data = create_new_candy_data_structure(friend_favorites)
+
+    # Act
+    result = create_candy_set(data)
+
+    # Assert
+    assert type(result) == set
+    assert len(result) == 8
