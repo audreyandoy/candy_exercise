e1cedb307fe7067855a0f6cc9336a85a81ca6bb3
Audrey Andoy <audreylandoy@gmail.com>
2021-09-14 08:00:36 -0700

    added test for data type of new data structure

--- tests/test_candy_data_structure.py
+++ tests/test_candy_data_structure.py
@@ -1,7 +1,22 @@
 import pytest
 from candy_problem.main import * 
 
-def test_create_candy_data_structure():
+def test_create_candy_data_structure_type():
+    # Arrange
+    friend_favorites = [
+        ["Sally", [ "lollipop”, “bubble gum", "laffy taffy"]],
+        [ "Bob", ["milky way", "licorice", "lollipop"]],
+        [ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+        [ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+    ]
+
+    # Act
+    new_candy_data = create_new_candy_data_structure(friend_favorites)
+    
+    # Assert
+    assert type(new_candy_data) == dict
+
+def test_create_candy_data_structure_values():
 
     # Arrange
     friend_favorites = [
@@ -15,7 +30,6 @@ def test_create_candy_data_structure():
     new_candy_data = create_new_candy_data_structure(friend_favorites)
     
     # Assert
-    assert type(new_candy_data) == dict   
     assert len(new_candy_data) == 8
     assert new_candy_data["lollipop"] == ["Sally", "Bob"]
 
