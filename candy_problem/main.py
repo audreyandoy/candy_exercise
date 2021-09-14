1c032bf492bb3bacd694dc6b0f05273a0b839158
Audrey Andoy <audreylandoy@gmail.com>
2021-09-13 18:06:31 -0700

    adds code solution for list of dictionaries

--- candy_problem/main.py
+++ candy_problem/main.py
@@ -6,8 +6,7 @@ a new data structure in the function `create_new_candy_data_structure`
 that describes the different types of candy paired with a list of friends that 
 like that candy. 
 
-2. In `get_favorite_candy_by_name()`, return the candies
-that Arlene likes.
+2. In `get_friends_who_like_candy_type()`, return friends who like lollipops.
 
 3. In, `create_candy_set()`, return a set of all the candies from
 the data structure made in `create_new_candy_data_structure()`.
@@ -44,23 +43,15 @@ def create_new_candy_data_structure(data):
         for candy in candy_list:
             if candy in new_data.keys():
                 new_data[candy].append(friend_name)
-
-    print(new_data)
     return new_data
-    
-
-create_new_candy_data_structure(friend_favorites)
 
 
 def get_friends_who_like_candy_type(data, type):
     if type is None or data is None:
         return "They dont even go here"
-
-    print(data[type])
     return data[type] 
     
-get_friends_who_like_candy_type(create_new_candy_data_structure(friend_favorites), "lollipop")
-
 
 def create_candy_set(data):
-    pass 
+    return set(list(data.keys()))
+
